09f3390e60d2e8af4145866239d10ad7399affe9
MariusBaldovin <youremailid@example.com>
2024-04-02 19:38:26 +0100

    solved task 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean, //added new state to control ghraph visibility
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, // show graph set to false
     };
   }
 
@@ -29,20 +31,37 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    //condition to prevent the graph render until the user clicks the button 'start streaming'
+    if (this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
+  }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
-  getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+   getDataFromServer() {
+    let intervalsCounter = 0; // Initialize intervals counter
+    
+    // Update showGraph state to true to render the graph
+    this.setState({ showGraph: true });
+  
+    // Use setInterval to repeatedly call the getData method every 100ms
+    // Set the interval ID to stop the interval later
+    const intervalId = setInterval(() => {
+      // Increment intervals counter
+      intervalsCounter++;
+      
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState({ data: [...this.state.data, ...serverResponds] });
+      });
+      
+      // Clear the interval after 1000 intervals
+      if (intervalsCounter === 1000) {
+        clearInterval(intervalId);
+      }
+    }, 100);
   }
-
   /**
    * Render the App react component
    */
